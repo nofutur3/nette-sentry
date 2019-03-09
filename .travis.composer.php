d959cc7abed3f2c384ac3dba46a9b9776dafcbcd
Jakub Vyvazil <jakub@vyvazil.cz>
2019-03-09 12:47:58 +0100

    fixed build @ travis

--- .travis.composer.php
+++ .travis.composer.php
@@ -10,10 +10,10 @@ echo "Nette version " . $version . PHP_EOL;
 
 $file = __DIR__ . '/composer.json';
 $content = file_get_contents($file);
-$composer  = json_decode($content, TRUE);
-if (!isset($composer['require']['nette/nette'])) {
+$composer  = \json_decode($content, true);
+if (!isset($composer['require']['nette/application'])) {
     exit(255);
 }
-$composer['require']['nette/nette'] = $version;
-$content = json_encode($composer);
+$composer['require']['nette/application'] = $version;
+$content = \json_encode($composer);
 file_put_contents($file, $content);
